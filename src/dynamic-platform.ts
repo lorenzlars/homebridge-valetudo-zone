b7f0e82129c052362644475207b26031f7b0b271
Lars Lorenz <mail@larslorenz.dev>
2022-10-09 16:20:51 +0200

    updated

--- src/dynamic-platform.ts
+++ src/dynamic-platform.ts
@@ -1,49 +1,52 @@
-import http, { IncomingMessage, Server, ServerResponse } from "http";
 import {
+  AccessoryPlugin,
   API,
-  APIEvent,
-  CharacteristicEventTypes,
-  CharacteristicSetCallback,
-  CharacteristicValue,
-  DynamicPlatformPlugin,
   HAP,
   Logging,
-  PlatformAccessory,
-  PlatformAccessoryEvent,
   PlatformConfig,
+  StaticPlatformPlugin,
 } from "homebridge";
 import axios from "axios";
-
-const PLUGIN_NAME = "homebridge-valetudo-zone";
-const PLATFORM_NAME = "ValetudoZone";
+import { ExampleSwitch } from "./switch";
 
 let hap: HAP;
 
 export = (api: API) => {
   hap = api.hap;
 
-  api.registerPlatform(PLATFORM_NAME, ExampleDynamicPlatform);
+  api.registerPlatform("ValetudoZone", ExampleDynamicPlatform);
 };
 
-class ExampleDynamicPlatform implements DynamicPlatformPlugin {
+class ExampleDynamicPlatform implements StaticPlatformPlugin {
+  private readonly ip: string;
+  private readonly log: Logging;
+
   constructor(log: Logging, config: PlatformConfig, api: API) {
+    this.log = log;
+
+    this.ip = config.ip;
+
     log.info("ValetudoZone platform finished initializing!");
-
-    api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
-      log.info("ValetudoZone platform 'didFinishLaunching'");
-
-      axios
-        .get(
-          `http://${config.ip}/api/v2/robot/capabilities/MapSegmentationCapability`
-        )
-        .then((response) => {
-          log.debug(response.data);
-
-          const uuid = api.hap.uuid.generate("SOMETHING UNIQUE");
-          const accessory = new api.platformAccessory("DISPLAY NAME", uuid);
-        });
-    });
   }
 
-  configureAccessory(accessory: PlatformAccessory): void {}
+  /*
+   * This method is called to retrieve all accessories exposed by the platform.
+   * The Platform can delay the response my invoking the callback at a later time,
+   * it will delay the bridge startup though, so keep it to a minimum.
+   * The set of exposed accessories CANNOT change over the lifetime of the plugin!
+   */
+  accessories(callback: (foundAccessories: AccessoryPlugin[]) => void): void {
+    axios
+      .get(
+        `http://${this.ip}/api/v2/robot/capabilities/MapSegmentationCapability`
+      )
+      .then((response) => {
+        this.log.info(response.data);
+      });
+
+    callback([
+      new ExampleSwitch(hap, this.log, "Switch 1"),
+      new ExampleSwitch(hap, this.log, "Switch 2"),
+    ]);
+  }
 }
