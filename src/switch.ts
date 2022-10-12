844cbe5656077f1636114112e78b582a9306ef54
Lars Lorenz <mail@larslorenz.dev>
2022-10-12 23:28:17 +0200

    updated

--- src/switch.ts
+++ src/switch.ts
@@ -3,7 +3,10 @@ import { MapSegmentationCapability } from "./dynamic-platform";
 import axios from "axios";
 
 export class SegmentSwitch implements AccessoryPlugin {
+  private readonly hap: HAP;
   private readonly log: Logging;
+  private readonly capability: MapSegmentationCapability;
+  private readonly ip: string;
 
   // This property must be existent!!
   name: string;
@@ -17,32 +20,26 @@ export class SegmentSwitch implements AccessoryPlugin {
     capability: MapSegmentationCapability,
     ip: string
   ) {
+    this.hap = hap;
     this.log = log;
+    this.capability = capability;
+    this.ip = ip;
+
     this.name = capability.name;
 
-    this.switchService = new hap.Service.Switch(`${capability.name} Clean`);
+    this.switchService = new this.hap.Service.Switch(
+      `${this.capability.name} Clean`
+    );
     this.switchService
-      .getCharacteristic(hap.Characteristic.On)
-      .onSet(async () => {
-        await axios.put(
-          `http://${ip}/api/v2/robot/capabilities/MapSegmentationCapability`,
-          {
-            action: "start_segment_action",
-            segment_ids: [capability.id],
-          }
-        );
+      .getCharacteristic(this.hap.Characteristic.On)
+      .onSet(this.setOnHandler.bind(this))
+      .onGet(this.getOnHandler.bind(this));
 
-        setTimeout(() => {
-          this.switchService.setCharacteristic(hap.Characteristic.On, false);
-        }, 2000);
-      })
-      .onGet(() => false);
+    this.informationService = new this.hap.Service.AccessoryInformation()
+      .setCharacteristic(this.hap.Characteristic.Manufacturer, "Valetudo")
+      .setCharacteristic(this.hap.Characteristic.Model, "Segment Clean");
 
-    this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "Valetudo")
-      .setCharacteristic(hap.Characteristic.Model, "Segment Clean");
-
-    log.info("Switch '%s' created!", `${capability.name} Clean`);
+    this.log.info("Switch '%s' created!", `${this.capability.name} Clean`);
   }
 
   /*
@@ -51,5 +48,26 @@ export class SegmentSwitch implements AccessoryPlugin {
    */
   getServices(): Service[] {
     return [this.informationService, this.switchService];
+  }
+
+  getOnHandler() {
+    return false;
+  }
+
+  async setOnHandler(value: any) {
+    await axios.put(
+      `http://${this.ip}/api/v2/robot/capabilities/MapSegmentationCapability`,
+      {
+        action: "start_segment_action",
+        segment_ids: [this.capability.id],
+      }
+    );
+
+    setTimeout(() => {
+      this.switchService.updateCharacteristic(
+        this.hap.Characteristic.On,
+        false
+      );
+    }, 5000);
   }
 }
